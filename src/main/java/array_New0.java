1e67157e4c0627ee1ec01b217864f6d3865c3d58
sanaz salemi <sanazsalemi9898>
2021-11-20 03:13:49 -0800

    project array_New0

--- src/main/java/array_New0.java
+++ src/main/java/array_New0.java
@@ -1,26 +1,19 @@
 public class array_New0 {
         public static void main(String[] args) {
-            int[] myArray = {1, 4, 7, 9};
-            String[] Str = {"one" , "four" ,"seven" ,"nine"};
+            int[] myArray = {1 ,4 ,7 , 9};
+            String[] Str = {"one" ,"four", "seven","nine"};
             for (int i = 0; i < myArray.length; i++) {
-                if (myArray[i] == 1) {
-                    System.out.println("Str[0]" + "=" + "One");
-                } else if (myArray[i] == 4) {
-                    System.out.println("Str[1]" + "=" + "Four");
-                } else if (myArray[i] == 7) {
-                    System.out.println("Str[2]" + "=" + "Seven");
-                } else if(myArray[i]==9){
-                    System.out.println("Str[3]" + "=" + "Nine");
-                }else {
+            if (myArray[i] == 1) {
+                System.out.println(Str[0]);
+            } else if (myArray[i] == 4) {
+                System.out.println(Str[1]);
+            } else if (myArray[i] == 7) {
+                System.out.println(Str[2]);
+            } else if (myArray[i] ==9 ) {
+                System.out.println(Str[3]);}
+                    else{
                     System.out.println("null");
-
-
-
-
-
                 }
             }
         }
     }
-
-
